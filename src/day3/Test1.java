a04dd615f3eb017a36895840b7ce0046dce3bc05
Kuugo <kuugo@kuugo.top>
2026-05-10 11:13:29 +0800

    添加项目基础结构和示例代码，包括 .gitignore、IDE 配置文件和 HelloWorld、Test1 示例类

---
+++ src/day3/Test1.java
@@ -0,0 +1,17 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个三位数");
+        int number = sc.nextInt();
+        int ones = number % 10;
+        int tens = number / 10 % 10;
+        int hundreds = number / 100;
+        System.out.println(ones);
+        System.out.println(tens);
+        System.out.println(hundreds);
+    }
+}
